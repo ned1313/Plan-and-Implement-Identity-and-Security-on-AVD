5c911bdaff4fdf9dedb1a311edd85f796aaf0efd
ned1313 <ned.bellavance@gmail.com>
2023-01-12 13:51:08 -0500

    fix link to directory

--- 6-add-Azure-Bastion/DeployAzureBastion.ps1
+++ 6-add-Azure-Bastion/DeployAzureBastion.ps1
@@ -10,8 +10,8 @@
 terraform init
 
 # Get the resource group and name of the Hub VNet
-$env:TF_VAR_hub_vnet_resource_group = terraform -chdir="..\1-create-domain-controller" output -raw vnet_resource_group
-$env:TF_VAR_hub_vnet_name = terraform -chdir="..\1-create-domain-controller" output -raw vnet_name
+$env:TF_VAR_hub_vnet_resource_group = terraform -chdir="..\1-deploy-lab-environment" output -raw vnet_resource_group
+$env:TF_VAR_hub_vnet_name = terraform -chdir="..\1-deploy-lab-environment" output -raw vnet_name
 
 # Set the IP address range for the Bastion subnet
 $env:TF_VAR_bastion_subnet_ip_range = "10.0.0.64/26"
